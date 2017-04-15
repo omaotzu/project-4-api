7e49bde1391daf8036eeee2e4a5698dd7606c275
mrkjdvs <mjd814@gmail.com>
2017-04-15 18:14:07 +0100

    trips and users linked

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   validates :username, presence: true
   validates :email, presence: true, uniqueness: true, unless: :oauth_login?
   validates :password, presence: true, confirmation: true, unless: :oauth_login?, on: :create
-  
+  has_many :trips
+
   def oauth_login?
     github_id.present?
   end
