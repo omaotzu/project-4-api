f5ae6ae4993b6df2acdfcde6d671152f0299ce6b
mrkjdvs <mjd814@gmail.com>
2017-04-15 20:48:27 +0100

    checking of relationships via rails console

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::API
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def current_user
     @current_user ||= User.find(decoded_token[:id]) if id_found?
