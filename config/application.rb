fbee742a5bfc5ad1503ad33779cea915fd4ae431
omaotzu <omarhphillips@gmail.com>
2017-04-16 19:04:29 +0100

    updated config for put and delete

--- config/application.rb
+++ config/application.rb
@@ -18,16 +18,14 @@ Bundler.require(*Rails.groups)
 
 module Project4
   class Application < Rails::Application
-
+    config.eager_load_paths << Rails.root.join('lib')
     config.api_only = true
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
-
-    config.eager_load_paths << Rails.root.join('lib')
   end
 end
